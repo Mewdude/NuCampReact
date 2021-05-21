f3047c4c8ccff9f39194de42baad7cf71e9d1290
Jonathan Arcocha <JonathanArcocha@gmail.com>
2021-05-20 22:08:45 -0400

    Presentational and Container Components

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -44,12 +44,16 @@ class CampsiteInfo extends Component {
         return (<div />)
     }
 
+
+
     render() {
         if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             )
         }
